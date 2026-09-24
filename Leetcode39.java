e1b6aaf70f56f6daeca368d3afdd997d1544388f
Sejalbansal2005 <158037667+Sejalbansal2005@users.noreply.github.com>
2026-09-24 18:58:43 +0530

    Implement combination Sum method using backtracking

---
+++ Leetcode39.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void backtrack(int[] candidates, int target, int start,
+                           List<Integer> curr, List<List<Integer>> ans) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] > target) continue;
+
+            curr.add(candidates[i]);
+
+            // i, not i + 1: same number can be reused
+            backtrack(candidates, target - candidates[i], i, curr, ans);
+
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
